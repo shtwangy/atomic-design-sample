e24312a706c092a5e7cc5b001c6e9de3500a0b99
shtwangy <shtwangy@gmail.com>
2021-01-03 19:27:52 +0900

    feat(src/components/atoms): create Balloon component

---
+++ src/components/atoms/Balloon/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles.css'
+
+const Balloon = () => {
+    return (
+        <span className={styles.balloon}>削除する</span>
+    )
+}
+
+export default Balloon
