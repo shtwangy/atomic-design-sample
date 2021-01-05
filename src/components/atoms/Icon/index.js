34286e3b966d6f36a2b740a217c11f0354d92a1d
shtwangy <shtwangy@gmail.com>
2021-01-05 13:01:52 +0900

    feat(src/components/atoms/Icon): fixed to allow TrashCanIcon component to be clickable

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -1,15 +1,23 @@
 import React from "react"
+import styles from "./styles.css"
 
-export const TrashCanIcon = ({
-    height = 20,
-    width = 20,
-    ...props
-}) => (
-    <img
-        src="/icons/trash-can.svg"
-        alt=""
-        height={height}
-        width={width}
-        {...props}
-    />
-)
+export const TrashCanIcon = (
+    {
+        height = 20,
+        width = 20,
+        className = '',
+        onClick,
+        ...props
+    }) => {
+    if (onClick) className += ` ${styles.clickable}`
+    return (
+        <img
+            src="/icons/trash-can.svg"
+            alt=""
+            height={height}
+            width={width}
+            className={className}
+            onClick={onClick}
+            {...props}
+        />)
+}
