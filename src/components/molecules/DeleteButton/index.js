f9df502bcf8c41738024e2fb0cf7fe80783fc298
shtwangy <shtwangy@gmail.com>
2021-01-12 21:26:23 +0900

    refactor(src/components/molecules/DeleteButton): use HoverTipInteraction

--- src/components/molecules/DeleteButton/index.js
+++ src/components/molecules/DeleteButton/index.js
@@ -2,12 +2,13 @@ import React from "react"
 import styles from './styles.css'
 import {TrashCanIcon} from "../../atoms/Icon";
 import Balloon from "../../atoms/Balloon";
+import HoverTipInteraction from "../../atoms/HoverTipInteraction";
 
 const DeleteButton = ({className, onClick, ...props}) => (
-    <span className={[styles.root, className].join(' ')} {...props}>
+    <HoverTipInteraction className={[styles.root, className].join(' ')} {...props}>
         <TrashCanIcon onClick={onClick} />
         <Balloon>削除する</Balloon>
-    </span>
+    </HoverTipInteraction>
 )
 
 export default DeleteButton
