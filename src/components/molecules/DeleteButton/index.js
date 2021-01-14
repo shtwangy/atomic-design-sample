d061cdfc15d758c9db54b45722cfe45b607508c0
shtwangy <shtwangy@gmail.com>
2021-01-15 07:49:19 +0900

    fix(src/components/molecules/DeleteButton): adjust tip display location

--- src/components/molecules/DeleteButton/index.js
+++ src/components/molecules/DeleteButton/index.js
@@ -2,12 +2,12 @@ import React from "react"
 import styles from './styles.css'
 import {TrashCanIcon} from "../../atoms/Icon";
 import Balloon from "../../atoms/Balloon";
-import HoverTipInteraction from "../../atoms/HoverTipInteraction";
+import HoverTipInteraction, {Tip} from "../../atoms/HoverTipInteraction";
 
 const DeleteButton = ({className, onClick, ...props}) => (
     <HoverTipInteraction className={[styles.root, className].join(' ')} {...props}>
         <TrashCanIcon onClick={onClick} />
-        <Balloon>削除する</Balloon>
+        <Tip><Balloon>削除する</Balloon></Tip>
     </HoverTipInteraction>
 )
 
