19fcafd2ebd07a965c806220a4766751ff53c47e
shtwangy <shtwangy@gmail.com>
2021-01-14 21:51:01 +0900

    refactor(src/components/organisms/Notification): use MediaObjectsLayout

--- src/components/organisms/Notification/index.js
+++ src/components/organisms/Notification/index.js
@@ -5,6 +5,7 @@ import Heading from "../../atoms/Heading"
 import {InfoTxt} from "../../atoms/Txt"
 import Time from '../../atoms/Time'
 import DeleteButton from "../../molecules/DeleteButton"
+import MediaObjectLayout from "../../atoms/MediaObjectLayout";
 
 // TODO: Functional Componentで書き換えてみる
 export class NotificationContainer extends Component {
@@ -27,26 +28,22 @@ export class NotificationContainer extends Component {
 }
 
 const NotificationPresenter = ({
-    program,
-    className,
-    onClickDelete,
-    ...props
-}) => (
-    <section className={[styles.root, className].join(' ')} {...props}>
-        <div>
-            <Img src={program.thumbnail} className={styles.media} width='128' height='72' />
-        </div>
-        <div className={styles.body}>
-            <Heading level={3} visualLevel={6}>{program.title}</Heading>
-            <InfoTxt size='s'>{program.channelName}</InfoTxt>
-            <InfoTxt size='s' className={styles.time}>
-                <Time format='MM月DD日(ddd)HH:mm'>{program.startAt}</Time>
-                〜
-                <Time format='HH:mm'>{program.endAt}</Time>
-            </InfoTxt>
-            <DeleteButton onClick={onClickDelete} className={styles.del} />
-        </div>
-    </section>
+                                   program,
+                                   className,
+                                   onClickDelete,
+                                   ...props
+                               }) => (
+    <MediaObjectLayout tag='section' className={[styles.root, className].join(' ')} {...props}>
+        <Img src={program.thumbnail} className={styles.media} width='128' height='72' />
+        <Heading level={3} visualLevel={6}>{program.title}</Heading>
+        <InfoTxt size='s'>{program.channelName}</InfoTxt>
+        <InfoTxt size='s' className={styles.time}>
+            <Time format='MM月DD日(ddd)HH:mm'>{program.startAt}</Time>
+            〜
+            <Time format='HH:mm'>{program.endAt}</Time>
+        </InfoTxt>
+        <DeleteButton onClick={onClickDelete} className={styles.del} />
+    </MediaObjectLayout>
 )
 
 const Notification = props => (
